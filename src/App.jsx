5f85de263cdfdb7faebe70e1d12ce3986b5d1479
bhagirathpaliyal <bhagirathpaliyal@gmail.com>
2024-11-10 14:02:40 +0530

    fixed : Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
-import Header from "./components/header/Header"
-import Home from "./Pages/Home"
-import AboutUs from "./Pages/AboutUs"
-import Footer from './components/Footer';
-
-
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./Pages/Home";
+import AboutUs from "./Pages/AboutUs";
+import Footer from "./components/Footer";
 
 function App() {
+ 
+  return (
+    <BrowserRouter>
+      <div className="container  mx-auto border">
+        <Header />
 
-
-  return ( 
-  <BrowserRouter >
-    <div className="container  mx-auto border">
-      <Header/>
-     
-      <Routes >
-        <Route  path="/" element={<Home/>}/>
-        <Route  path="/About-Us" element={<AboutUs/>}/>
-      </Routes>
-      <Footer/>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About-Us" element={<AboutUs />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/header/ResponsiveNav.jsx
+++ src/components/header/ResponsiveNav.jsx
@@ -1,38 +1,74 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const ResponsiveNav = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className='md:hidden'>
-      <button className={`${isOpen ? '' : 'hidden'}`} onClick={() => (setIsOpen(prev => !prev))}>menu</button>
-      <button className={`${isOpen ? 'hidden' : ''} flex justify-end`} onClick={() => (setIsOpen(prev => !prev))}>
-        <img src="./src/assets/xmark-solid.svg" alt="X-Mark" className='w-[20px] flex justify-center items-center' />
+    <div className="md:hidden">
+      <button
+        className={`${isOpen ? "hidden" : ""}`}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        menu
       </button>
-      <div className={`${isOpen ? 'hidden' : ''} w-[50%] h-[100vh] bg-[#fff] absolute top-[68px] left-[0] flex flex-col`}>
-
-<nav className='m-[5%]'>
-
-  <ul className='flex flex-col gap-[20px]'>
-  <NavLink to={'/'} style={({ isActive }) => {
-          return isActive ? { color: "#f4b50e" } : { color: "#000" };
-        }}> <li>Home</li></NavLink>
-        <NavLink to={'/About-Us'} style={({ isActive }) => {
-          return isActive ? { color: "#f4b50e" } : { color: "#000" };
-        }} >   <li>About Us</li></NavLink>
-        <NavLink to={'/'} style={({ isActive }) => {
-          return isActive ? { color: "#f4b50e" } : { color: "#000" };
-        }}>   <li>Case Study</li></NavLink>
-        <NavLink to={'/About-Us'} style={({ isActive }) => {
-          return isActive ? { color: "#f4b50e" } : { color: "#000" };
-        }}>   <li>Contact Us</li></NavLink>
-
-  </ul>
-</nav>
-
+      <button
+        className={`${isOpen ? "" : "hidden"} flex justify-end`}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <img
+          src="../src/assets/xmark-solid.svg"
+          alt="X-Mark"
+          className="w-[20px] flex justify-center items-center"
+        />
+      </button>
+      <div
+        className={`${
+          isOpen ? "" : "hidden"
+        } w-[50%] h-[100vh] bg-[#fff] absolute top-[68px] left-[0] flex flex-col`}
+      >
+        <nav className="m-[5%]">
+          <ul className="flex flex-col gap-[20px]">
+            <NavLink
+              to={"/"}
+              style={({ isActive }) => {
+                return isActive ? { color: "#f4b50e" } : { color: "#000" };
+              }}
+            >
+              {" "}
+              <li onClick={() => setIsOpen((prev) => !prev)}>Home</li>
+            </NavLink>
+            <NavLink
+              to={"/About-Us"}
+              style={({ isActive }) => {
+                return isActive ? { color: "#f4b50e" } : { color: "#000" };
+              }}
+            >
+              {" "}
+              <li onClick={() => setIsOpen((prev) => !prev)}>About Us</li>
+            </NavLink>
+            <NavLink
+              to={"/"}
+              style={({ isActive }) => {
+                return isActive ? { color: "#f4b50e" } : { color: "#000" };
+              }}
+            >
+              {" "}
+              <li onClick={() => setIsOpen((prev) => !prev)}>Case Study</li>
+            </NavLink>
+            <NavLink
+              to={"/About-Us"}
+              style={({ isActive }) => {
+                return isActive ? { color: "#f4b50e" } : { color: "#000" };
+              }}
+            >
+              {" "}
+              <li onClick={() => setIsOpen((prev) => !prev)}>Contact Us</li>
+            </NavLink>
+          </ul>
+        </nav>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResponsiveNav
+export default ResponsiveNav;
