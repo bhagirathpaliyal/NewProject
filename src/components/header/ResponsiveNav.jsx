9132a36d3dbcfd68c14bdca5746169ffe6735e41
bhagirathpaliyal <bhagirathpaliyal@gmail.com>
2024-12-18 17:59:46 +0530

    fix: Responsiveness in navbar

--- src/components/header/ResponsiveNav.jsx
+++ src/components/header/ResponsiveNav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import xLogo from '/public/assets/xmark-solid.svg'
+import xLogo from "/public/assets/xmark-solid.svg";
 const ResponsiveNav = ({ scrollToSection }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
@@ -11,42 +11,41 @@ const ResponsiveNav = ({ scrollToSection }) => {
       >
         menu
       </button>
-      <button
-        className={`${isOpen ? "" : "hidden"} flex justify-end`}
-        onClick={() => setIsOpen((prev) => !prev)}
-      >
-        <img
-          src={xLogo}
-          alt="X-Mark"
-          className="w-[20px] flex justify-center items-center"
-        />
-      </button>
+      
       <div
         className={`${
           isOpen ? "" : "hidden"
-        } w-[50%] h-[100vh] bg-[#fff] absolute top-[68px] left-[0] flex flex-col`}
+        } w-[50%] h-[100vh] bg-[#fff] fixed top-[0px] left-[0] flex flex-col bg-gradient-to-r from-[#08132f] to-[#0c2b59]`}
       >
+        <button
+          className={`${isOpen ? "" : "hidden"} flex justify-end m-[10px]`}
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <img
+            src={xLogo}
+            alt="X-Mark"
+            className="w-[20px] flex justify-center items-center"
+          />
+        </button>
+
         <nav className="m-[5%]">
           <ul className="flex flex-col gap-[20px]">
-          <button onClick={() =>scrollToSection('Home') }>
-          {" "}
-          <li>Home</li>
-        </button>
-        <button onClick={() =>scrollToSection('About-Us') }>
-          {" "}
-          <li>About Us</li>
-        </button>
-        <button onClick={() =>scrollToSection('Case-Study') }>
-          {" "}
-          <li>Case Study</li>
-        </button>
-        <button
-          onClick={() =>scrollToSection('Contact-Us') }
-        >
-          {" "}
-          <li>Contact Us</li>
-        </button>
-    
+            <button onClick={() => scrollToSection("Home")}>
+              {" "}
+              <li>Home</li>
+            </button>
+            <button onClick={() => scrollToSection("About-Us")}>
+              {" "}
+              <li>About Us</li>
+            </button>
+            <button onClick={() => scrollToSection("Case-Study")}>
+              {" "}
+              <li>Case Study</li>
+            </button>
+            <button onClick={() => scrollToSection("Contact-Us")}>
+              {" "}
+              <li>Contact Us</li>
+            </button>
           </ul>
         </nav>
       </div>
